4ac7a0e270fbfc1342a592ca6a28b595a5fcdcf2
Sitharahansamali <hsithara69@gmail.com>
2026-03-11 11:07:28 +0530

    changed the code helping function to split reviews into batches

--- src/analyse-text/text-anlysis.py
+++ src/analyse-text/text-anlysis.py
@@ -19,32 +19,34 @@ def load_reviews(folder="reviews"):
 
     for file in os.listdir(folder):
         path = os.path.join(folder, file)
-
         with open(path, "r", encoding="utf-8") as f:
             reviews.extend(f.readlines())
 
     return [r.strip() for r in reviews if r.strip()]
 
 
+# Helper function to split reviews into batches of max n
+def chunks(lst, n):
+    """Yield successive n-sized chunks from list"""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
 def analyze_reviews():
     reviews = load_reviews()
 
-    # Detect language
-    languages = client.detect_language(reviews)
+    for batch in chunks(reviews, 10):  # Batch of max 10
+        sentiments = client.analyze_sentiment(batch)
+        languages = client.detect_language(batch)
 
-    # Sentiment analysis
-    sentiments = client.analyze_sentiment(reviews)
-
-    for review, lang, sent in zip(reviews, languages, sentiments):
-
-        print("\nReview:", review)
-        print("Detected Language:", lang.primary_language.name)
-        print("Language Code:", lang.primary_language.iso6391_name)
-
-        print("Sentiment:", sent.sentiment)
-        print("Positive:", sent.confidence_scores.positive)
-        print("Neutral:", sent.confidence_scores.neutral)
-        print("Negative:", sent.confidence_scores.negative)
+        for review, lang, sent in zip(batch, languages, sentiments):
+            print("\nReview:", review)
+            print("Detected Language:", lang.primary_language.name)
+            print("Language Code:", lang.primary_language.iso6391_name)
+            print("Sentiment:", sent.sentiment)
+            print("Positive:", sent.confidence_scores.positive)
+            print("Neutral:", sent.confidence_scores.neutral)
+            print("Negative:", sent.confidence_scores.negative)
 
 
 if __name__ == "__main__":
